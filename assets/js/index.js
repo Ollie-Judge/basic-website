dbd37b87aac099876cca5af2339dd52ce7bc5e20
Ollie-Judge <ollie.judge@hotmail.com>
2022-04-07 16:26:35 +0100

    added colour randomiser and styled the home divs

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+let colours = ["blue", "yellow", "green", "pink", "orange", "red", "#fffafa"];
+
+let colourChange = document.getElementById("change");
+
+colourChange.addEventListener("click", function () {
+  var randomColour = colours[Math.floor(Math.random() * colours.length)];
+
+  let bodyChange = document.getElementById("body");
+
+  bodyChange.style.background = randomColour;
+});
